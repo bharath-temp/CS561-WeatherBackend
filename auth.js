868545d3e5e34c88be2b4a5e633890bee1d6acb0
Bharath Padmaraju <bharath.k.padmaraju@gmail.com>
2022-02-01 15:26:19 -0800

    feat(api): enable endpoints to use auth

--- auth.js
+++ auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const request = require('request')
+const http = require('http')
 
 app.use(bodyParser.json());
 
@@ -19,6 +21,38 @@ const users = [
 
 app.listen(3000, () => {
     console.log('Auth service')
+});
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+app.get('/v1/weather', authenticateJWT, (req, res) => {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=corvallis&" +
+              "appid=a99f1dbe7d7e7d5b6ce85970a31da042"
+    request(url, (error, response, body)=>{
+        console.log(body);
+        res.send(JSON.parse(body));
+    });
+});
+
+app.get('/v1/hello', authenticateJWT, (req, res) => {
+    const greeting = "Howdy Partner";
+    res.json(greeting);
 });
 
 app.post('/v1/auth', (req, res) => {
